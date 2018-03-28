32ffc82a58f8b5c3eaf55bdfd192ddaef39db31f
yexudong <ye850570138@sina.com>
2018-03-28 18:42:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jme3.app.AndroidHarness;
+
+public class MainActivity extends AndroidHarness {
+    public MainActivity() {
+        this.appClass = "com.example.administrator.myapplication.HelloJME3";
+    }
+}
